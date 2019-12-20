896b7a5f7ecec2fbaec13c7754ae27fdbf0aa4e4
Spam-B0t <56346455+Spam-B0t@users.noreply.github.com>
2019-12-20 07:55:13 +0200

    Update main_debug.c

--- i8080/main_debug.c
+++ i8080/main_debug.c
@@ -3,6 +3,15 @@
 int main() {//j 1500
     cpu8080 i8080;
     reset(&i8080);
+    /*load(&i8080, "cpudiag.bin", 0x100);
+    //modify start:
+    i8080.memory[0x0005]=0xc3;
+    i8080.memory[0x6]=0;
+    i8080.memory[0x7]=0x01;
+    //skip ac test:
+    i8080.memory[0x59c] = 0xc3;
+    i8080.memory[0x59d] = 0xc2;
+    i8080.memory[0x59e] = 0x05;*/
     load(&i8080, "invaders.h", 0);
     load(&i8080, "invaders.g", 0x800);
     load(&i8080, "invaders.f", 0x1000);
